22aaa8f0daed51633a79911c3c46f9f7a5b323cf
Marcello Ghiozzi <m.ghiozzi@switchup.it>
2019-08-13 14:03:38 +0200

    More code types support: qr, aztec, code39, code93, code128, ean8, ean13, upce

--- src/android/QRScanner.java
+++ src/android/QRScanner.java
@@ -456,6 +456,13 @@ public class QRScanner extends CordovaPlugin implements BarcodeCallback {
                 //Configure the decoder
                 ArrayList<BarcodeFormat> formatList = new ArrayList<BarcodeFormat>();
                 formatList.add(BarcodeFormat.QR_CODE);
+                formatList.add(BarcodeFormat.AZTEC);
+                formatList.add(BarcodeFormat.CODE_39);
+                formatList.add(BarcodeFormat.CODE_93);
+                formatList.add(BarcodeFormat.CODE_128);
+                formatList.add(BarcodeFormat.EAN_8);
+                formatList.add(BarcodeFormat.EAN_13);
+                formatList.add(BarcodeFormat.UPC_E);
                 mBarcodeView.setDecoderFactory(new DefaultDecoderFactory(formatList, null, null));
 
                 //Configure the camera (front/back)
